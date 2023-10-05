8d6d46b608c0a807dc5d0948a4cf00051e0be152
Rohit Bharti <rohitbharti279@gmail.com>
2023-10-05 14:11:32 +0530

    search

--- index.js
+++ index.js
@@ -6,47 +6,67 @@ const product = require("./product.js");
 const app = express();
 app.use(express.json());
 
-//post = Save new data
-app.post("/create", async (req, res) => {
-    const data = new product(req.body);
-    const result = await data.save();
-    console.log(result);
-    res.send(result);
-});
+// //post = Save new data
+// app.post("/create", async (req, res) => {
+//     const data = new product(req.body);
+//     const result = await data.save();
+//     console.log(result);
+//     res.send(result);
+// });
 
-//list of data
-app.get("/list", async (req, res) => {
-    const data = await product.find();
-    res.send(data);
-});
+// //list of data
+// app.get("/list", async (req, res) => {
+//     const data = await product.find();
+//     res.send(data);
+// });
 
-//delete of data
-app.delete("/delete/:_id", async (req, res) => {
-    // console.log(req.params);
-    // res.send("deleted Successfully");
+// //delete of data
+// app.delete("/delete/:_id", async (req, res) => {
+//     // console.log(req.params);
+//     // res.send("deleted Successfully");
 
-    const data = await product.deleteOne(req.params);
-    res.send(data);
-    console.log(data);
-});
+//     const data = await product.deleteOne(req.params);
+//     res.send(data);
+//     console.log(data);
+// });
 
-//update of data
-app.put("/update/:_id", async (req, res) => {
+// //update of data
+// app.put("/update/:_id", async (req, res) => {
 
-    const data = await product.updateOne(
-        //     {name:"Cricket"}, 
-        // {
-        //     $set: {price: 999, category: "World cup 2023"}
-        // }
+//     const data = await product.updateOne(
+//         //     {name:"Cricket"}, 
+//         // {
+//         //     $set: {price: 999, category: "World cup 2023"}
+//         // }
 
-        //updated by postman
-        req.params,
+//         //updated by postman
+//         req.params,
+//         {
+//             $set: req.body
+//         }
+//     );
+//     res.send(data);
+//     console.log(data);
+// });
+
+
+
+
+//Search
+app.get("/search/:key", async (req, res) => {
+    console.log(req.params.key);
+    let searchData = await product.find(
         {
-            $set: req.body
+            "$or": [
+                //searchng by .....______
+                {"name": {$regex:req.params.key}},
+                {"brand": {$regex:req.params.key}},
+                {"category": {$regex:req.params.key}}
+            ]
         }
-    );
-    res.send(data);
-    console.log(data);
-});
+    )
+    res.send(searchData);
+    
+})
 
 app.listen(8080);
