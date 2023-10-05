69a202015212b96c62b7f2473bfd0b22977c3512
Rohit Bharti <rohitbharti279@gmail.com>
2023-10-05 13:20:18 +0530

    post

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const product = require("./product.js");
 const app = express();
 app.use(express.json());
 
+//post = Save new data
 app.post("/create", async (req,res) => {
     const data = new product(req.body);
     const result = await data.save();
